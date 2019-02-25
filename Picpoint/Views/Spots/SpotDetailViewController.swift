8951c6aa74ca78c9278e6eec614acf5504195c19
alumnos <sofia_santos_apps1ma1718@cev.com>
2019-02-25 10:25:28 +0100

    Vistas Detalle Spot v1

--- Picpoint/Views/Spots/SpotDetailViewController.swift
+++ Picpoint/Views/Spots/SpotDetailViewController.swift
@@ -4,14 +4,27 @@ import UIKit
 
 class SpotDetailViewController: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var spotName: UILabel!
+    @IBOutlet weak var author: UILabel!
+    @IBOutlet weak var spotDescription: UILabel!
+    @IBOutlet weak var createdBy: UILabel!
+   
+    
     var spot = Spot()
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        image.image = spot.image
+        spotName.text = spot.name
+        spotDescription.text = spot.desc
+        
      
     }
     
-
+    @IBAction func showInMapButton(_ sender: Any) {
+        
+    }
     
 
 }
